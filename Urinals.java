f258f1e3a0a7853e6c71c5477264e06c0b47f9e6
padired1 <padired1@asu.edu>
2022-10-27 14:27:42 -0700

    Testing if the given string is binary or not,with alphabets as input-Failed test case

--- Urinals.java
+++ Urinals.java
@@ -3,6 +3,20 @@ package org.example;
   Prathyusha Adireddi
 */
 public class Urinals{
+    String input;
+    public String getInput()
+    {
+        return input;
+    }
+    public boolean goodString(String str){
+        if(str.matches("[01][01]+"))
+        {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
     public static void main(String[] args) {
 
     }
--- UrinalsTest.java
+++ UrinalsTest.java
@@ -1,5 +1,17 @@
 package org.example;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class UrinalsTest {
+    Urinals u=new Urinals();
+
+    @Test
+    public void testString()
+    {
+        String res= "Prathyu";
+        assertEquals(true,u.goodString(res));
+        System.out.println("======PRATHYUSHA==TEST ONE EXECUTED========");
+    }
+
 
 }
