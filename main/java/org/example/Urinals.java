358990589d993b9436b6460f4472552140f38438
padired1 <padired1@asu.edu>
2022-10-27 21:53:28 -0700

    implemented the code for file input

--- main/java/org/example/Urinals.java
+++ main/java/org/example/Urinals.java
@@ -31,20 +31,34 @@ public class Urinals {
         }
 
     }
+    public void write_To_File(String optfile,int uri_cnt){
+        try {
+            FileWriter w = new FileWriter(optfile, true);
+            BufferedWriter bufwri=new BufferedWriter(w);
+            bufwri.write(Integer.toString(uri_cnt));
+            bufwri.newLine();
+            bufwri.close();
+        }
+        catch(IOException e){
+            System.out.println("Error in opening output file");
+            e.printStackTrace();
+        }
+    }
     public void open_File(String filepath) {
         try{
             Urinals u1=new Urinals();
             File f=new File(filepath);
             Scanner sc=new Scanner(f);
+            String optFile="src/rule.txt";
             while(sc.hasNextLine()){
                 String str=sc.nextLine();
-                if(str.equals("-1")){
-                    break;}
+                if(str.equals("-1")) break;
                 int uri_cnt = u1.countUrinals(str);
-
+                u1.write_To_File(optFile,uri_cnt);
                 System.out.println(uri_cnt);
                 }
         }
+
         catch(IOException e)
         {
             System.out.println("Error in opening");
